4eecc9eee1e192a7b0d53708c4d9a10babce3489
AugustineMacharia <39903725+AugustineMacharia@users.noreply.github.com>
2018-07-31 08:40:01 +0000

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -22,7 +22,20 @@
 		<input type="password" name="password" placeholder="password" >
 	</div>
 	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="confpass" placeholder="Re enter the password">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" placeholder="Email" value="">
+	</div>
+	<div class="input-group">
 		<button type="submit" class="btn btn-primary" name="Login_btn">Login</button>
+	</div>
+	<div class="input-group">
+		<label>Remeber me</label>
+		<input type="checkbox" name = "remember" value = "1">
+		
 	</div>
 	<p>
 		Forgoten password? <a href="forgotpasswordpage.php">Reset password</a><br>
